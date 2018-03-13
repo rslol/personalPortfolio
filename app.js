6088d6fb2430bd9d8886d3cc3a2c04ab05c1b6d9
rslol <richard.shelton567@gmail.com>
2018-03-13 17:45:18 -0400

    Updates

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
   var express = require('express'),
       app = express(),
       bodyParser = require('body-parser'),
-      mongoose   = require('mongoose'),
       methodOverride = require('method-override'),
       expressSanitizer = require('express-sanitizer');
      
 
-mongoose.connect('mongodb://localhost/personal_portfolio');
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(expressSanitizer());
-
-
-var portfolioSchema = new mongoose.Schema ({
-  title: String,
-  image: String,
-  body: String,
-  author: String, 
-  created: {type: Date, default: Date.now}
-});
-
-var Portfolio = mongoose.model('Portfolio', portfolioSchema);
 
 //Landing
 app.get('/', function(req,res){
@@ -49,10 +37,6 @@ app.get('/contact', function(req,res){
 })
 
 
-
-
-
-
-app.listen(3000, function(){
-  console.log("Portfolio connected");
+app.listen(3000, '0.0.0.0', function(){
+  console.log("Listening to port: " + 3000);
 });
